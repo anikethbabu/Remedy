f83368e9465180d206e06839a8f2f839909e9538
Behzod <74157130+Wolverine1122@users.noreply.github.com>
2021-03-28 20:52:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+function initMap() {
+  const myLatLng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatLng,
+  });
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+}
